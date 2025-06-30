df6b8bb92b5839b631a238dee7ed4cd77f7051c7
Rallentando456 <144925979+raulaine@users.noreply.github.com>
2025-06-30 08:13:03 -0300

    aula 6

--- main.js
+++ main.js
@@ -24,15 +24,35 @@ function aumentaTamanho() {
 }
 
 const campoSenha = document.querySelector('#campo-senha');
+const checkbox = document.querySelectorAll('.checkbox')
+
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numero = '0123456789';
+const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha() {
-    let senha = '' ;
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    } 
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numero;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+
+        let senha = '' ;
     for(let i=0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+        let numeroAleatorio = Math.random()*alfabeto.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
-        senha = senha + letrasMaiusculas[numeroAleatorio];
+        senha = senha + alfabeto[numeroAleatorio];
     }
     campoSenha.value = senha; 
 }
